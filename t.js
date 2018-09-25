38c05cd60e5d2f2c80c94a8d37259eb1a837cf93
Jacques COUVREUR <jacques@2ia.net>
2018-09-26 00:57:38 +0200

    #1 - init project as CLI.

---
+++ t.js
@@ -0,0 +1,64 @@
+const json1jsonFile = 'timy-jco.json';
+
+var arguments = require('minimist')(process.argv.slice(2));
+var fs = require('fs');
+var moment = require('moment');
+
+const now = new Date();
+var json = JSON.parse(fs.readFileSync(json1jsonFile, 'utf8'));
+
+function stopCurrent(json, now) {
+    const previous = json.current;
+    if (previous && previous.project) {
+        previous.stop = now;
+        json.tracks = json.tracks || [];
+        json.tracks.push(previous);
+        json.current = {};
+    }
+}
+
+function formatDuration(start, stop) {
+    const seconds = (stop.getTime() - start.getTime()) / 1000;
+    return moment.utc(moment.duration(seconds, "s").asMilliseconds()).format("HH:mm:ss");
+}
+
+if (arguments.alias || arguments.a) {
+    const a = arguments.alias || arguments.a;
+    const tokens = a.split(':');
+    const alias = tokens[0];
+    const project = tokens[1];
+    json.aliases = json.aliases || {};
+    json.aliases[alias] = project;
+}
+
+if (arguments.start) {
+    const alias = arguments.start;
+    const project = json.aliases[alias] || alias;
+
+    stopCurrent(json, now);
+
+    json.current = {
+        "start": now,
+        "project": project
+    };
+}
+
+if (arguments.stop) {
+    stopCurrent(json, now);
+}
+
+if (arguments.help || arguments.sos || arguments.usage || process.argv.length == 2) {
+    console.log('$ node t --alias|a alias:project --start aliasOrProject --stop --report|r');
+}
+
+if (arguments.report || arguments.r) {
+    if (json.current && json.current.project) {
+        const duration = formatDuration(new Date(json.current.start), now);
+        console.log('  - Project :', json.current.project);
+        console.log('  - Duration:', duration + 's');
+    } else {
+        console.log('  - No pending task!');
+    }
+}
+
+fs.writeFileSync(json1jsonFile, JSON.stringify(json), 'utf8');
