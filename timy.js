4e36c5917ac7c0ec1ed4716511aa09c0cd802650
Jacques COUVREUR <jacques@2ia.net>
2018-09-26 23:39:57 +0200

    closes #5 - Add comment on current track: bugfix if no argument.

--- timy.js
+++ timy.js
@@ -14,13 +14,13 @@ module.exports = {
     },
 
     handleComment: function (json, arguments, errorCallback) {
-        if (json && json.current && json.current.project) {
-            if (arguments.comment || arguments.c) {
+        if (arguments.comment || arguments.c) {
+            if (json && json.current && json.current.project) {
                 json.current.comments = json.current.comments || [];
                 json.current.comments.push(arguments.comment || arguments.c);
+            } else if (errorCallback) {
+                errorCallback(' /!\\ Can not add comment because there is no current track.');
             }
-        } else if (errorCallback) {
-            errorCallback(' /!\\ Can not add comment because there is no current track.');
         }
     },
 
