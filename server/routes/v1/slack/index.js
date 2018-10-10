d70363f136c571e00407ab195b0a69dc24d20441
Jacques COUVREUR <jacques@2ia.net>
2018-10-11 00:35:03 +0200

    #20 - Setup Slack App: slack request params.

--- server/routes/v1/slack/index.js
+++ server/routes/v1/slack/index.js
@@ -11,3 +11,17 @@ router.use('/', actions);
 router.use('/', options);
 
 module.exports = router;
+
+const body = {
+    "token": "jTsNQ5HnQ9aKdFg6Rmw6vx47",
+    "team_id": "T1623MCPR",
+    "team_domain": "inagua",
+    "channel_id": "DDAT53KPX", // @TIMy channel
+    "channel_name": "directmessage",
+    "user_id": "U1635ND18",
+    "user_name": "jacques",
+    "command": "/start",
+    "text": "gtechna",
+    "response_url": "https://hooks.slack.com/commands/T1623MCPR/455010441350/AY279dAq59CCIAOn19cQAYKy",
+    "trigger_id": "453474199908.40071726807.2c2c0087b3f6165e3ce3661830cd8742"
+};
