20e8f547666d693a9e0d876324c528c4f7d712f3
Jacques <jacques@2ia.net>
2018-10-10 12:48:20 +0200

    aws codepipeline

--- server/routes/v1/health.js
+++ server/routes/v1/health.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', function(req, res, next) {
-    res.json({ message: 'Welcome on TIMy!... I am really healthy ;)' });
+    res.json({ message: 'Welcome on TIMy!... I am healthy ;)' });
 });
 
 module.exports = router;
