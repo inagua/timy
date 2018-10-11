f17da2702b1137ffa3367ddb025f6a29a61012e4
inagua <mail@inagua.ch>
2018-10-11 15:00:06 +0200

    aws codepipeline trigger.

--- server/routes/v1/health.js
+++ server/routes/v1/health.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', function(req, res, next) {
-    res.json({ message: 'Welcome on TIMy!... I am VERY healthy ;)' });
+    res.json({ message: 'Welcome on TIMy!... I am healthy++ ;)' });
 });
 
 module.exports = router;
