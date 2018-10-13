26d2a38b10421b021c8ee8405dfdc23452d113dc
Jacques COUVREUR <jacques@2ia.net>
2018-10-14 00:22:54 +0200

    #20 - Setup Slack App: Events API route, url validation.

--- server/routes/v1/slack/events.js
+++ server/routes/v1/slack/events.js
@@ -2,7 +2,14 @@ var express = require('express');
 var router = express.Router();
 
 router.post('/events', function(req, res) {
-    res.json({ message: 'events!', body: JSON.stringify(req.body) });
+
+    // https://api.slack.com/events/url_verification
+    if (req.body && req.body.type === "url_verification") {
+        res.send(req.body.challenge);
+
+    } else {
+        res.json({ message: 'events!', body: JSON.stringify(req.body) });
+    }
 });
 
 module.exports = router;
