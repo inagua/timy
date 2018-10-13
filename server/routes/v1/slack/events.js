b95fa57369cb713d5f9df6a15091d29cbd2dce19
Jacques COUVREUR <jacques@2ia.net>
2018-10-14 00:51:30 +0200

    #20 - Setup Slack App: Events API route, chat.postMessage.

--- server/routes/v1/slack/events.js
+++ server/routes/v1/slack/events.js
@@ -1,6 +1,11 @@
 var express = require('express');
 var router = express.Router();
 
+const { WebClient } = require('@slack/client');
+const token = process.env.SLACK_TOKEN || 'jTsNQ5HnQ9aKdFg6Rmw6vx47';
+const web = new WebClient(token);
+
+
 router.post('/events', function(req, res) {
 
     // https://api.slack.com/events/url_verification
@@ -8,8 +13,41 @@ router.post('/events', function(req, res) {
         res.send(req.body.challenge);
 
     } else {
-        res.json({ message: 'events!', body: JSON.stringify(req.body) });
+        console.log('>>>>> ', req.body);
+        // res.json({ message: 'events!', body: JSON.stringify(req.body) });
+// This argument can be a channel ID, a DM ID, a MPDM ID, or a group ID
+        const conversationId = req.body.channel || 'C1232456';
+// See: https://api.slack.com/methods/chat.postMessage
+        web.chat.postMessage({ channel: conversationId, text: JSON.stringify(req.body) })
+            .then((res) => {
+                // `res` contains information about the posted message
+                console.log('Message sent: ', res.ts);
+                res.sendStatus(200);
+            })
+            .catch(console.error);
     }
 });
 
 module.exports = router;
+
+
+
+/*
+const { WebClient } = require('@slack/client');
+
+// An access token (from your Slack app or custom integration - xoxa, xoxp, or xoxb)
+const token = process.env.SLACK_TOKEN;
+
+const web = new WebClient(token);
+
+// This argument can be a channel ID, a DM ID, a MPDM ID, or a group ID
+const conversationId = 'C1232456';
+
+// See: https://api.slack.com/methods/chat.postMessage
+web.chat.postMessage({ channel: conversationId, text: 'Hello there' })
+  .then((res) => {
+    // `res` contains information about the posted message
+    console.log('Message sent: ', res.ts);
+  })
+  .catch(console.error);
+ */
